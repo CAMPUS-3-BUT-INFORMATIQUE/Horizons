1b18d10b4021df2cb00f761da2c5817a8e5caf2a
Alexandre Angot <alexandre.angot@etu.unicaen.fr>
2024-04-15 19:03:58 +0200

    fix(sidebar): fix accordion behavior Refs: #7

--- src/resources/views/admin/sidebar.blade.php
+++ src/resources/views/admin/sidebar.blade.php
@@ -4,6 +4,7 @@
     </div>
     @foreach ($menus as $menu => $submenus)
         <div tabindex="{{ $loop->index }}" class="collapse collapse-arrow hover:bg-gray-200/20 my-3">
+            <input type="radio" name="accordion" />
             <div class="collapse-title flex items-center font-bold text-xl">
                 {{ $menu }}
             </div>
