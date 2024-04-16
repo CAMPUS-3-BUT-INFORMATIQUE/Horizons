6c44c2cb13a69a9671c32863bf0f5e4986bafb03
romain.lecouvreur <lecouvreurromain@gmail.com>
2024-04-16 16:13:57 +0200

    🔀 Merge

--- src/database/migrations/2024_04_15_134322_create_reports_table.php
+++ src/database/migrations/2024_04_15_134322_create_reports_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\ReportType;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -18,11 +17,11 @@ class CreateReportsTable extends Migration
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class, 'user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-            $table->foreignIdFor(ReportType::class, 'report_type_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->dateTime('start_date');
             $table->dateTime('end_date');
-            $table->string('title');
-            $table->text('report_agenda');
+            $table->string('report_agenda');
+            $table->text('decisions');
+            $table->text('conclusions');
             $table->text('comments');
             $table->timestamps();
         });
