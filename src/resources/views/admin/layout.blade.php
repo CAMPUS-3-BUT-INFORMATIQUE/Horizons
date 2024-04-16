d6377e4b9e4c38f629f3769d92b48876658c0771
Théo PEREIRA <theo.pereira@etu.unicaen.fr>
2024-04-16 15:01:37 +0200

    cicd: test

--- src/resources/views/admin/layout.blade.php
+++ src/resources/views/admin/layout.blade.php
@@ -53,6 +53,7 @@
                 </div>
                 <button class="bg-gray-400 hover:bg-gray-300 text-white font-bold py-2 px-4 rounded">
                     Se déconnecter
+
                 </button>
             </div>
             <div id="content">
