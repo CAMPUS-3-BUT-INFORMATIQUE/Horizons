8d59557aa522bd765862514f1e02cbbc227f4684
Théo Duval <theo.duval27@proton.me>
2024-04-16 17:08:10 +0200

    Update 2

--- src/app/Http/Controllers/DecisionsRecordController.php
+++ src/app/Http/Controllers/DecisionsRecordController.php
@@ -6,12 +6,15 @@ use Barryvdh\Debugbar\Controllers\BaseController;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\View\View;
 
-class DecisionsRecordController extends BaseController {
+class DecisionsRecordController extends BaseController
+{
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function index() {
-        return view('decisionsRecords', ['color' => 'Sblue', "sectionName" => "Relevés des décisions"]);
+    public function index(): View
+    {
+        return view('decisionsRecords', ['color' => 'blue', "sectionName" => "Relevés des décisions"]);
 
     }
 }
