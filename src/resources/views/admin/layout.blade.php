ad5b4041b734956b7591c1c202bc9f1d400389d1
Théo PEREIRA <theo.pereira@etu.unicaen.fr>
2024-04-16 13:18:27 +0200

    feat(navigation): add home button on admin IHM

--- src/resources/views/admin/layout.blade.php
+++ src/resources/views/admin/layout.blade.php
@@ -41,8 +41,16 @@
             @endforeach
         </div>
         <div class="flex flex-col w-5/6">
-            <div class="flex justify-between p-4 mb-2 bg-gray-500/30">
-                <div></div>
+            <div class="flex items-center justify-between p-4 mb-2 bg-gray-500/30">
+                <div>
+                    <a href="/" class="rounded-md bg-gray-600/20 hover:bg-gray-900/20 px-3 py-2 text-white transition-all duration-300 flex items-center justify-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+                            <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                            <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+                        </svg>
+                        <span class="font-bold">Accueil</span>
+                    </a>
+                </div>
                 <button class="bg-gray-400 hover:bg-gray-300 text-white font-bold py-2 px-4 rounded">
                     Se déconnecter
                 </button>
