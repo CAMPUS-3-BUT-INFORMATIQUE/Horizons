3682f243c2f4aa71b23f4ea6f2d028ada77fcce4
antoine <piioupioupiiouu@gmail.com>
2024-04-16 09:53:08 +0200

    Place logo well

--- src/resources/views/components/header.blade.php
+++ src/resources/views/components/header.blade.php
@@ -1,42 +1,45 @@
 @php
     $navs = [
-        ["title" => 'FAQ', 'href' => '#'],
-        ["title" => 'Poursuite Et.', 'href' => '#'],
-        ["title" => 'Stages', 'href' => '#'],
-        ["title" => 'Apprentissage', 'href' => '#'],
-        ["title" => 'Relevé Décisions', 'href' => '#'],
-        ["title" => 'Chef Départ.', 'href' => '#'],
-        ["title" => 'Doc. Officiels', 'href' => '#'],
+        ['title' => 'FAQ', 'href' => '#'],
+        ['title' => 'Poursuite Et.', 'href' => '#'],
+        ['title' => 'Stages', 'href' => '#'],
+        ['title' => 'Apprentissage', 'href' => '#'],
+        ['title' => 'Relevé Décisions', 'href' => '#'],
+        ['title' => 'Chef Départ.', 'href' => '#'],
+        ['title' => 'Doc. Officiels', 'href' => '#'],
     ];
 @endphp
 
 <div>
     <header class='shadow-md bg-white font-[sans-serif] min-h-[70px] min-w-full'>
-        <div class='flex items-center mx-auto gap-5 relative w-fit'>
-          <a href="/">
-            <img src="{{ asset('images/UNICAEN_LOGO_noir.svg') }}" alt="logo" class='w-36' />
-          </a>
+        <div class='flex items-center gap-5 relative w-full lg:px-5'>
+            <a href="/" class="min-w-36">
+                <img src="{{ asset('images/UNICAEN_LOGO_noir.svg') }}" alt="logo" class='w-36' />
+            </a>
 
-          <ul id="collapseMenu" class='lg:!flex max-lg:w-full'>
-            @foreach ($navs as $nav)
-                <li class='max-lg:border-b max-lg:bg-[#007bff] py-3 px-7 max-lg:rounded border-solid border-2 min-w-32 '>
-                <a href="{{$nav['href']}}"
-                    class='lg:hover:text-[#007bff] max-lg:text-white block font-semibold text-m grid justify-items-center'>{{$nav['title']}}</a>
-                </li>
-            @endforeach
+            <div class="mx-auto">
+                <ul class='lg:!flex max-lg:w-full'>
+                    @foreach ($navs as $nav)
+                        <li class='py-3 px-7 border-solid border-2 min-w-32 '>
+                            <a href="{{ $nav['href'] }}"
+                                class='hover:text-[#007bff]  block font-semibold text-m grid justify-items-center'>{{ $nav['title'] }}</a>
+                        </li>
+                    @endforeach
 
-            @auth('admin')
-                <li class='max-lg:border-b max-lg:bg-[#007bff] py-3 px-7 max-lg:rounded border-solid border-2 min-w-32 '>
-                <a href="#"
-                    class='lg:hover:text-[#007bff] max-lg:text-white block font-semibold text-m grid justify-items-center'>Admin</a>
-                </li>
-            @endauth
+                    {{-- admin --}}
+                    @auth
+                        <li class='py-3 px-7 border-solid border-2 min-w-32 '>
+                            <a href="#"
+                                class='hover:text-[#1c73a9] block font-semibold text-m grid justify-items-center'>Admin</a>
+                        </li>
+                    @endauth
 
-          </ul>
+                </ul>
+            </div>
 
-          <a href="/">
-            <img src="{{ asset('images/couleur.svg') }}" alt="logo" class='w-36' />
-          </a>
+            <a href="/" class="min-w-36">
+                <img src="{{ asset('images/couleur.svg') }}" alt="logo" class='w-36' />
+            </a>
         </div>
-      </header>
+    </header>
 </div>
