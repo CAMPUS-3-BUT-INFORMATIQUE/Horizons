e48d3f96683dc19e8dcf018ed1d3f4fe6d0fc4be
Nyriox <Nyriox@users.noreply.github.com>
2024-04-16 13:13:05 +0000

    Prettified Code!

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-      content: [
+    content: [
         "./resources/views/**/*.{html,js,php}",
         "./resources/js/**/*.{js}",
     ],
-      theme: {
+    theme: {
         screens: {
             "1xl5": "1340px",
         },
-            extend: {
+        extend: {
             colors: {
                 "horizon-yellow": "#ffcd49",
                 "horizon-cyan": "#1adbe1",
                 "horizon-blue": "#1c73a9",
             },
         },
-      },
-      plugins: [require("daisyui")],
+    },
+    plugins: [require("daisyui")],
 };
