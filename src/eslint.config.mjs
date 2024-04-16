dababc28dfa09a61ad9f101b78948044f53f64ee
Nehocute <nehocuk_@hotmail.com>
2024-04-16 14:12:42 +0200

    yarn run eslint fix

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -9,12 +9,12 @@ const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
 
 export default [
- {
+  {
     files: ['**/*.js'],
-    languageOptions: { sourceType: 'commonjs' },
- },
- {
+    languageOptions: { sourceType: 'commonjs' }
+  },
+  {
     ignores: ['/config/*', 'tailwind.config.js', 'webpack.mix.js']
- },
- ...compat.extends('standard')
-];
+  },
+  ...compat.extends('standard')
+]
